bd2d78138f23866e498e7ff6a2365d19d15c9487
Steven Espinoza <espisteven5@gmail.com>
2023-12-11 16:56:29 -0800

    done

--- lib/not_none_functions.py
+++ lib/not_none_functions.py
@@ -1,2 +1,2 @@
-def return_not_none():
-    return None
+def return_true():
+    return True
--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-from not_none_functions import return_not_none
+from not_none_functions import return_true
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+
+def test_return_true():
+    """in not_none_functions, function "return_true" returns a value that is True."""
+    result = return_true()
+    assert result is True
